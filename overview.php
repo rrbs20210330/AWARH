8e8d9c829c6f1d98d3097dca2e4fc2887ef9e31d
Alan9092 <106710642+Alan9092@users.noreply.github.com>
2022-06-13 22:38:17 -0500

    Add files via upload

--- overview.php
+++ overview.php
@@ -1,41 +1,71 @@
 <?php include('components/header.php') ?>
-<head>
-<link rel="stylesheet" href="assets/css/style.css">
-</head>
+<header>
+  <link rel ="stylesheet" href="assets/css/style.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js"></script>
+</header>
 <body>
-<label>
-<div class="card" style="width: 18rem;">
-  <img src="https://cdn-icons-png.flaticon.com/512/305/305992.png" class="card-img-top" width="120" height="200" alt="InternetCtr">
-  <div class="card-body">
-    <h5 class="card-title">Empleado</h5>
-    <p class="card-text"></p>
-    <a href="#" class="btn btn-primary">Entrar</a>
-</label>
+  <div class="container">
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <center>
+    <a href="employees.php"><i class="fa-5x bi bi-person-circle"></i></a>
+</center>
+    <div class="card-body">
+        <h5 class="card-title">Empleados</h5>
+        <p class="card-text">Cantidad de empleados:</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Ver mas</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <center>
+      <a href="training.php"><i class="fa-5x bi bi-person-bounding-box"></i></a>
+</center>
+      <div class="card-body">
+        <h5 class="card-title">Capacitaciones</h5>
+        <p class="card-text">Cantidad de capacitaciones:</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Ver mas</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <center>
+    <a href="announcements.php"><i class="fa-5x bi bi-file-earmark-image"></i></a>
+</center>  
+    <div class="card-body">
+        <h5 class="card-title">Convocatorias</h5>
+        <p class="card-text">Cantidad de convocatorias</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Ver mas</small>
+      </div>
+    </div>
+  </div>
 </div>
+<div style="width:50%">
+  <canvas id="grafica" width="1" height="1"></canvas>
 </div>
-<div class="card" style="width: 18rem;">
-  <img src="https://cdn-icons-png.flaticon.com/512/305/305992.png" class="card-img-top" width="120" height="200" alt="InternetCtr">
-  <div class="card-body">
-    <h5 class="card-title">Candidatos</h5>
-    <p class="card-text"></p>
-    <a href="#" class="btn btn-primary">Entrar</a>
-</div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="https://cdn-icons-png.flaticon.com/512/305/305992.png" class="card-img-top" width="120" height="200" alt="InternetCtr">
-  <div class="card-body">
-    <h5 class="card-title">Convocatorias</h5>
-    <p class="card-text"></p>
-    <a href="#" class="btn btn-primary">Entrar</a>
-</div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="https://cdn-icons-png.flaticon.com/512/305/305992.png" class="card-img-top" width="120" height="200" alt="InternetCtr">
-  <div class="card-body">
-    <h5 class="card-title">Formularios</h5>
-    <p class="card-text"></p>
-    <a href="#" class="btn btn-primary">Entrar</a>
-</div>
-</div>
-</body>
-<?php include('components/footer.php') ?>
+
+<script>
+  var grafica = document.getElementById("grafica");
+  var myPirChart = new Chart(grafica,{
+    type: 'pie',
+    data: {
+      labels: ['Empleados','Candidatos','Convocatorias'],
+      datasets: [{
+        label: "Reportes",
+        data:[20,30,10],
+        backgroundColor: ["#00ced1","#ee82ee","#ffa500"]
+      }]
+    },
+  });
+  </script>
+  </body>
+<?php include('components/footer.html') ?>
